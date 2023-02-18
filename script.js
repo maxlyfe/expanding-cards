6b588d15b3d10f6c1df0a2de355c479c7dde05d7
MaxLyFe <maximilianolyfbuzios@gmail.com>
2023-02-17 21:53:16 -0300

    adicionando comentarios no codigo

--- script.js
+++ script.js
@@ -1,5 +1,8 @@
+/* Seleccionando todos los elementos con el panel de clases. */
 const panels = document.querySelectorAll('.panel')
 
+/* Agregar un detector de eventos a cada panel. Cuando se hace clic en el panel, se eliminará la clase
+activa de todos los paneles y se agregará la clase activa al panel en el que se hizo clic. */
 panels.forEach(panel => {
     panel.addEventListener('click', () => {
         removeActiveClasses()
@@ -7,6 +10,9 @@ panels.forEach(panel => {
     })
 })
 
+/**
+ * Elimina la clase activa de todos los paneles.
+ */
 function removeActiveClasses() {
     panels.forEach(panel => {
         panel.classList.remove('active')
